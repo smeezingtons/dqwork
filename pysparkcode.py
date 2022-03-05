fcc95740e2d062d833fdc5da2be1a09fe58e7844
smeezingtons <patilankit50@gmail.com>
2022-03-05 23:02:17 +0530

    change4

--- pysparkcode.py
+++ pysparkcode.py
@@ -1,6 +1,15 @@
 #from boto3 import client
+from awsglue.context import GlueContext
+from pyspark.context import SparkContext
 from pyspark import SparkConf
 from pyspark.sql import SparkSession
+
+sc = SparkContext()
+glueContext = GlueContext(sc)
+logger = glueContext.get_logger()
+
+logger.warn("warn message")
+logger.error("error message")
 
 my_conf = SparkConf()
 my_conf.set("spark.app.name","myfirstapplication")
@@ -10,8 +19,8 @@ spark = SparkSession.builder.config(conf=my_conf).getOrCreate()
 
 data = [1,2,3,4,5,6,7,8,9,10]
 rdd = spark.sparkContext.parallelize(data)
-print(rdd.collect())
 
+logger.info(rdd.collect())
 
 spark.stop()
 
